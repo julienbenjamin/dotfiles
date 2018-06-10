681ccb5b58d88144b6437df49f4a8c5d1cc0da10
julienbenjamin <julienbenjamin@.free.fr>
2018-06-10 17:41:20 +0200

    refactor: add description and group to autofocus command

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,7 @@
 -- Automatically reload a file on external changes
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  desc = "Reload file when gaining focus",
+  group = vim.api.nvim_create_augroup("reload-focus-retrieved", { clear = true }),
   callback = function()
     if vim.fn.mode() ~= "c" then
       vim.cmd("checktime")
