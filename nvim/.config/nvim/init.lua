a872429df2fe500d9fffc0632f5ac48d1bcdd651
julienbenjamin <julienbenjamin@.free.fr>
2018-06-10 18:45:10 +0200

    clean: remove unecessary commented list of plugins which are already handled by lazy

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,7 +5,4 @@ require('config.autocmds')
 require('config.keymaps')
 require('config.lsp')
 require('config.options')
---require('plugins.blink')
---require('plugins.colorscheme')
---require('plugins.lsp')
---require('plugins.mason')
+
