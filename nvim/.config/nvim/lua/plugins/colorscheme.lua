869aa46f179d948598b996e1623545f4494d091d
julienbenjamin <julienbenjamin@.free.fr>
2018-06-11 17:50:20 +0200

    feat: add nightfox and removed italics from nightfox and monokai

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,30 @@
 return {
   { "folke/tokyonight.nvim" },
-  { "tanvirtin/monokai.nvim" },
+  {
+    "tanvirtin/monokai.nvim",
+
+    config = function()
+      require("monokai").setup {
+        palette = require("monokai").pro,
+	italics = false
+    }
+    end
+  },
+  {
+    "EdenEast/nightfox.nvim",
+
+    config = function()
+      require('nightfox').setup({
+	-- remove italics
+	groups = {
+          all = {
+            Keyword = { style = "NONE" },
+	    ["@keyword"] = { style = "NONE" },
+	    ["@conditional"] = { style = "NONE" },
+	    ["@repeat"] = { style = "NONE" },
+	  },
+	},
+    })
+    end,
+  },
 }
