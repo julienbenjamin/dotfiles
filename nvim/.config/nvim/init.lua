a6542e35a3ab28524702afd156f59e0742a33e94
julienbenjamin <julienbenjamin@.free.fr>
2018-06-11 15:55:30 +0200

    fix: update import order so current configuration works properly

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,8 @@
 package.path = vim.fn.stdpath("config") .. "/lua/?.lua;" .. package.path
 
+require('config.options')
 require('config.lazy')
 require('config.autocmds')
 require('config.keymaps')
 require('config.lsp')
-require('config.options')
 
