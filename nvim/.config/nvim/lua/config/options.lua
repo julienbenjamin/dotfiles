7c50b0291dc90743f78b22df602ffef584727ee4
julienbenjamin <julienbenjamin@.free.fr>
2018-06-11 14:18:40 +0200

    feat: remove whitespace and tabs handling from neovim itself

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,10 +1,10 @@
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+--vim.opt.tabstop = 4
+--im.opt.softtabstop = 4
+--vim.opt.shiftwidth = 4
+--vim.opt.expandtab = true
 vim.opt.number = true
 --vim.opt.relativenumber = true
 vim.opt.cursorline = false
